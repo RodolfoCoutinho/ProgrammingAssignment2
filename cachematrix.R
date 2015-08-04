e14f493f0b570bfcd98f87f34813820c28c69657
Rodolfo <rodolfo__coutinho@hotmail.com>
2015-08-04 15:50:33 -0300

    Commiting for peer assessments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,30 @@
-## Put comments here that give an overall description of what your
-## functions do
+## This functions are used for calculating and caching the inverse of a matrix.
 
-## Write a short comment describing this function
+## Creates a special matrix that can store the inverse of the input matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setsolve <- function(solve) m <<- solve
+    getsolve <- function() m
+    list(set = set, get = get,
+         setsolve = setsolve,
+         getsolve = getsolve)
 }
 
 
-## Write a short comment describing this function
+## Return the cached inverse if the matrix is the same or calculate the new inverse.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getsolve()
+    if(!is.null(m)) {
+        return(m)
+    }
+    m <- solve(x$get())
+    x$setsolve(m)
+    m
 }
